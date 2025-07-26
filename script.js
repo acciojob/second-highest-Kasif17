7d6cfcc5860ae77ec7697289ad5b895ba1b672dd
Mohd Kasif Khan <developer786kasif@gmail.com>
2025-07-26 13:27:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function secondHighest(arr) {
+  let max = -Infinity;
+  let second = -Infinity;
+
+  for (let num of arr) {
+    if (num > max) {
+      second = max;
+      max = num;
+    } else if (num < max && num > second) {
+      second = num;
+    }
+  }
+
+  return second === -Infinity ? -Infinity : second;
+}
+console.log(secondHighest()); 
